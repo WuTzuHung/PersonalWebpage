bbf485d24e1a800955fb28d943f7d738e195e70d
WuTzuHung <not5508085@gmail>
2024-10-20 00:56:52 +0800

    wowCorrect

--- src/main.js
+++ src/main.js
@@ -59,43 +59,17 @@ barba.init({
             },
 
             async enter(data) {
-                // 每次進入首頁時，執行動畫
-                if (data.next.namespace === "home") {
-                    restartBackgroundAnimation();
-                }
+
                 contentAnimation(); // 其他頁面過渡動畫
             },
 
             async once(data) {
-                // 首次進入頁面時的動畫
-                if (data.next.namespace === "home") {
-                    restartBackgroundAnimation();
-                }
+
                 contentAnimation();
             },
         },
     ],
 });
-
-// GSAP 動畫控制函數
-function restartBackgroundAnimation() {
-    const backgroundAnimation = document.getElementById("backgroundAnimation");
-    if (!backgroundAnimation) return;
-
-    // 重設背景動畫的透明度和顯示狀態
-    backgroundAnimation.style.display = "block";
-    backgroundAnimation.style.opacity = 1;
-
-    // 播放淡出動畫
-    gsap.to("#backgroundAnimation", {
-        duration: 1,    // 動畫持續時間
-        delay: 3.5,     // 延遲時間
-        opacity: 0,     // 最終透明度
-        onComplete: function() {
-            backgroundAnimation.style.display = "none"; // 動畫結束後隱藏
-        }
-    });
-}
 
 document.addEventListener('DOMContentLoaded', function() {
     const homeLis = document.querySelectorAll('li[data-page="home"]');
